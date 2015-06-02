6a3a6f96f5133ceeb3a7a191eba3a898c7412bc1
Nick Leach <nickleach22@gmail.com>
2015-06-02 16:45:19 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$.getJSON('http://private-44f6d-timeline6.apiary-mock.com/timeline').done( function(data){
+  var stuff = data.timelines;
+    stuff.forEach( function(x){
+    var dates = x.timestamp;
+    var m = moment(new Date(dates));
+          x.date= m.format('MM/DD/YY');
+          x.times= m.format('hh:mm');
+    });
+  $('.container').html(template.timelines(data));
+});
+
---
+++ js/templates.js
@@ -0,0 +1,22 @@
+this["template"] = this["template"] || {};
+this["template"]["timelines"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
+
+  return "      <div class=\"dates\">"
+    + alias3(((helper = (helper = helpers.date || (depth0 != null ? depth0.date : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"date","hash":{},"data":data}) : helper)))
+    + "</div>\n      <div class=\"times\">"
+    + alias3(((helper = (helper = helpers.times || (depth0 != null ? depth0.times : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"times","hash":{},"data":data}) : helper)))
+    + "</div>\n      <li class=\"icons\"><i class=\"fa "
+    + alias3(((helper = (helper = helpers.icon || (depth0 != null ? depth0.icon : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"icon","hash":{},"data":data}) : helper)))
+    + "\"></i></li>\n      <li class=\"mainstuff\">\n      <h4>"
+    + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
+    + "</h4>\n      <section>"
+    + alias3(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"content","hash":{},"data":data}) : helper)))
+    + "</section>\n      </li>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1;
+
+  return "\n<ul class\"ul-right\">\n"
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.timelines : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "</ul>\n";
+},"useData":true});
