00a8062c10936bd5fdd43a25060543f58275b3fb
Nick Leach <nickleach22@gmail.com>
2015-06-02 22:17:10 -0400

    Finished

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 $.getJSON('http://private-44f6d-timeline6.apiary-mock.com/timeline').done( function(data){
   var stuff = data.timelines;
     stuff.forEach( function(x){
--- js/templates.js
+++ js/templates.js
@@ -2,17 +2,17 @@ this["template"] = this["template"] || {};
 this["template"]["timelines"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "      <div class=\"topstuff\">\n      <div class=\"dates\">"
+  return "      <div class=\"topstuff\">\n        <div class=\"dates\">"
     + alias3(((helper = (helper = helpers.date || (depth0 != null ? depth0.date : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"date","hash":{},"data":data}) : helper)))
-    + "</div>\n      <div class=\"times\">"
+    + "</div>\n        <div class=\"times\">"
     + alias3(((helper = (helper = helpers.times || (depth0 != null ? depth0.times : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"times","hash":{},"data":data}) : helper)))
-    + "</div>\n      <div class=\"icons\"><i class=\"fa "
+    + "</div>\n        <div class=\"icons\">\n          <i class=\"fa "
     + alias3(((helper = (helper = helpers.icon || (depth0 != null ? depth0.icon : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"icon","hash":{},"data":data}) : helper)))
-    + "\"></i></div>\n      </div>\n      <li class=\"mainstuff\">\n      <h4>"
+    + "\"></i>\n        </div>\n      </div>\n      <li class=\"mainstuff\">\n        <section>\n          <article>\n            <h4>"
     + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</h4>\n      <section>"
+    + "</h4>\n            <p>"
     + alias3(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"content","hash":{},"data":data}) : helper)))
-    + "</section>\n      </li>\n";
+    + "</p>\n          </article>\n        </section>\n      </li>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
